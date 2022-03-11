3536fda5b3b31ee18e05bce81f4f014936c71e3c
Dominik Gruntz <dominik.gruntz@fhnw.ch>
2022-03-11 08:44:53 +0100

    adds Direction.java

--- 04_Exceptions/src/main/java/ch/fhnw/oop/enums/Direction.java
+++ 04_Exceptions/src/main/java/ch/fhnw/oop/enums/Direction.java
@@ -17,5 +17,18 @@ public enum Direction {
 		for (Direction dir : Direction.values()) {
 			System.out.println(dir + "/" + dir.ordinal() + "/" + dir.getAngle());
 		}
+
+		// Initialisierung einer Variablen mit einem bestimmten Wert
+		Direction d = Direction.EAST;
+
+		// Switch-Anweisung über die Werte eines Enums:
+		// Interessant: Wenn ein Label fehlt gibt der Compiler eine Warnung aus
+		//              (ausser natürlich es hätte ein Defulat-Label im switch)
+		switch(d) {
+			case NORTH: System.out.println("Norden"); break;
+			case EAST: System.out.println("Osten"); break;
+			case SOUTH: System.out.println("Süden"); break;
+			case WEST: System.out.println("Westen"); break;
+		}
 	}
 }
