e66318c790b894f665ddfec6c0915631e7057930
Dominik Gruntz <dominik.gruntz@fhnw.ch>
2022-03-15 09:30:05 +0100

    adds the solutions of the enum worksheet

--- 04_Exceptions/src/main/java/ch/fhnw/oop/enums/Gastronorm.java
+++ 04_Exceptions/src/main/java/ch/fhnw/oop/enums/Gastronorm.java
@@ -1,21 +1,39 @@
 package ch.fhnw.oop.enums;
 
 public class Gastronorm {
-//	  private Size size;
-//	  private Material material;
-//
-//	  public Gastronorm(Material material, Size size) {
-//	     this.material = material;
-//	     this.size = size;
-//	  }
-//
-//	  public Material getMaterial() { return material; }
-//	  public int getWidth() { return size.getWidth(); }
-//	  public int getLength() { return size.getLength(); }
-//	  public int getLiterVolume() { return size.getWidth() * size.getLength() / 10000;}
-//
-//	  // Enum Material
-//
-//	  // Enum Size, all measurements in millimeters
-//	}
+	private Size size;
+	private Material material;
+
+	public Gastronorm(Material material, Size size) {
+		this.material = material;
+		this.size = size;
+	}
+
+	public Material getMaterial() { return material; }
+	public int getWidth() { return size.getWidth(); }
+	public int getLength() { return size.getLength(); }
+	public double getLiterVolume() { return size.getWidth()*size.getLength()/10_000.0;}
+
+	// Enum Material
+	enum Material {
+		STEEL, PLASTIC
+	}
+
+	// Enum Size, all measurements in millimeters
+	enum Size {
+		GN1_4(162, 265),
+		GN1_2(325, 265),
+		GN1_1(325, 530);
+
+		private final int width;
+		private final int length;
+
+		private Size(int width, int length) {
+			this.width = width;
+			this.length = length;
+		}
+
+		public int getWidth() { return width; }
+		public int getLength() { return length; }
+	}
 }
--- 04_Exceptions/src/main/java/ch/fhnw/oop/enums/GastronormTest.java
+++ 04_Exceptions/src/main/java/ch/fhnw/oop/enums/GastronormTest.java
@@ -1,10 +1,13 @@
 package ch.fhnw.oop.enums;
 
-//import ch.fhnw.oop.enums.Gastronorm.Material;
-//import static ch.fhnw.oop.enums.Gastronorm.Material.*;
-//
-//public class GastronormTest {
-//	public static void main(String[] args) {
-//		Material m = STEEL;
-//	}
-//}
+import ch.fhnw.oop.enums.Gastronorm.Material;
+import static ch.fhnw.oop.enums.Gastronorm.Material.*;
+
+public class GastronormTest {
+	public static void main(String[] args) {
+		Material m = STEEL;
+
+        Gastronorm gn = new Gastronorm(m, Gastronorm.Size.GN1_1);
+        System.out.println(gn.getLiterVolume());
+	}
+}
